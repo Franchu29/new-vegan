ac61455a0e06fc19b5c781409c30812469c3cd80
ryuuseinan <130932226+ryuuseinan@users.noreply.github.com>
2025-06-02 11:37:15 -0400

    actualizar seleccion categoria

--- screens/PlatosApi.js
+++ screens/PlatosApi.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from '../config';
 export async function getEventos() {
   try {
-    const res = await fetch(`${API_BASE_URL}/api/platos`);
+    const res = await fetch(`${API_BASE_URL}/api/tipos_platos`);
     const data = await res.json();
     return data;
   } catch (error) {
